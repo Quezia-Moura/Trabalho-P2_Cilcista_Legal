6d8671e673f668d69479a5d84a000e79593c989f
Quézia Moura <125207561+Quezia-Moura@users.noreply.github.com>
2024-12-04 01:05:39 -0300

    Update reports.js

--- backend/routes/reports.js
+++ backend/routes/reports.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   try {
-    const { description, userId } = req.body;
-    const newReport = new Report({ description, userId });
+    const { description, infractionType, location, userId } = req.body; 
+    const newReport = new Report({ description, infractionType, location, userId });
     await newReport.save();
     res.status(201).json({ message: 'Denúncia criada com sucesso!', report: newReport });
   } catch (error) {
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
 router.get('/:userId', async (req, res) => {
   try {
     const reports = await Report.find({ userId: req.params.userId });
-    res.status(200).json(reports);
+    res.status(200).json(reports);  // Retorna todos os campos do relatório
   } catch (error) {
     res.status(400).json({ error: 'Erro ao buscar denúncias.' });
   }
