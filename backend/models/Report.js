947011fd0cb75b7d8098b9c0c71c67057f104d34
Quézia Moura <125207561+Quezia-Moura@users.noreply.github.com>
2024-12-04 01:03:25 -0300

    Update Report.js

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-const ReportSchema = new mongoose.Schema({
+const reportSchema = new mongoose.Schema({
   description: { type: String, required: true },
+  infractionType: { type: String, required: true }, 
+  location: { type: String, required: true },        
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Report', ReportSchema);
+const Report = mongoose.model('Report', reportSchema);
+
+module.exports = Report;
